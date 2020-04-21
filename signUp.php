6b6c8bf75ccc76b59cf399a775cd461d88e1ade0
yuuki <st061959@m03.kyoto-kcg.ac.jp>
2020-04-21 12:12:57 +0900

    ログイン機能と新規登録画面

---
+++ signUp.php
@@ -0,0 +1,68 @@
+<?php
+session_start();
+if (isset($_SESSION['name'])) {
+  echo 'ようこそ' . htmlspecialchars($_SESSION['name'], ENT_QUOTES, 'utf-8'). "さん<br>";
+  echo "<a href='/logout.php'>ログアウトはこちら。</a>";
+  exit;
+}
+   if(isset($_SESSION['erros'])){
+    $errors=$_SESSION['erros'];//入力エラーを変数に格納するため
+   }
+   if(isset($_SESSION['erross'])){
+    $errorss= $_SESSION['erross'];
+   }
+   
+?>
+<!DOCTYPE html>
+<html lang="ja">
+ <head>
+     <meta charset="utf-8">
+     <title>Login</title>
+ </head>
+ <body>
+
+<?php if ( isset( $_SESSION['name'] ) ):  ?>
+  <p><br><?='ようこそ' . htmlspecialchars( $_SESSION['name'], ENT_QUOTES, 'utf-8' )?>さん</br></p>
+  <a href='/logout.php'>ログアウトはこちら。</a>
+  <?= exit; ?> 
+<?php endif ?>
+
+<h1>ようこそ、ログインしてください。</h1>
+<form  action="roguin.php" method="post">
+<?php if ( isset($errorss) ):  ?>
+  <?php foreach ($errorss as $error):?>
+     <p><?=htmlspecialchars( $error, ENT_QUOTES, 'utf-8' )//入力エラーの表示?></p>
+  <?php endforeach;?>
+<?php endif ?>
+     <br><label for="userID">userID</label></br>
+     <br><input type="text" name="userid"></br>
+     <br><label for="password">password</label></br>
+     <br><input type="text" name="password"></br>
+     <input type="submit" name="send" value="Sign in!" class="button">
+   </form>
+
+   <h1>初めての方はこちら</h1>
+   <form action="register.php" method="post">
+   <?php if ( isset($errors) ):  ?>
+     <?php foreach ($errors as $error):?>
+      <p><?=htmlspecialchars( $error, ENT_QUOTES, 'utf-8' )//入力エラーの表示?></p>
+     <?php endforeach;?>
+   <?php endif ?>
+     <br><label for="username">名前</label></br>
+    <br><input type="text" name="username"></br>
+     <br><label for="password">password</label></br>
+     <br><input type="password" name="password"></br>
+     <input type="submit" name="send" value="Sign Up!" class="button">
+     <p>※パスワードは半角英数字をそれぞれ１文字以上含んだ、5文字以上で設定してください。</p>
+     <a href="logout.php">ログアウト</a>
+   </form>
+ </body>
+</html>
+
+
+
+
+
+
+
+
