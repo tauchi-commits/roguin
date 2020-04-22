657185da6f741eb2d01bb710ea42b8a74026c67e
yuuki <st061959@m03.kyoto-kcg.ac.jp>
2020-04-22 22:25:41 +0900

    ログアウト機能

--- logout.php
+++ logout.php
@@ -15,3 +15,16 @@ if (ini_get("session.use_cookies")) {//cookie値があるなら
 $_SESSION = array();
 
 ?>
+<!DOCTYPE html>
+<head>
+<meta charset="utf-8">
+<title>ログイン画面</title>
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <link  href="assets/css/style.css"  rel="stylesheet" media="all"  />
+  <body>
+  <a href=signUp.php>戻る</a>
+  </body>
+</html>
